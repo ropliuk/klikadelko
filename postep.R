71be2c0e2539215d2e068bf1c7b7737b0d2425f8
olekz <olekz@olekz.roam.corp.google.com>
2017-10-29 17:42:07 +0100

    Zapomniany plik: postep.R
    [[g31]]

---
+++ postep.R
@@ -0,0 +1,17 @@
+postep_start = function(n, msg='Ładuję dane') {
+  postep <<- shiny::Progress$new()
+  postep$set(message = msg, value = 0)
+  postep.max <<- n
+  print(paste('PS', n))
+  postep
+}
+
+postep_krok = function(dane=NULL, p, i=1, msg='') {
+  print(paste('PK', i, msg))
+  p$inc(i/postep.max, detail = msg)
+  dane
+}
+
+postep_koniec = function(p) {
+  p$close()
+}
